aa5ac00ef03ad9974ab34f7480e39a188e212ddc
Grace Jang <you@example.com>
2023-11-19 20:33:50 -0600

    second commit

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # weather-app
 
 Used the OpenWeatherMap API to build a weather app from scratch. I used the GeoCoding API to find the latitude and longitude of the user's city input, then returned weather information by using the latitude and longitude data.
+
+#second commit
+1. I drafed the design of the app and wrote the CSS for the first page. Included layout, designing the radio buttons for the unit selectors, and more.
+2. Added logic to the search function so that the user would be prompted to re-enter the cityif an invalid city was entered.
+3. Added unit logic - user can specify whether they want the results in imperial units or metric units.
+4. Added logic to the search function so that if the user searched without specifying the unit, they would be asked to select a unit.
--- script.js
+++ script.js
@@ -2,18 +2,26 @@ const p = document.querySelector('#paragraph');
 const cityInput = document.querySelector('#city');
 const form = document.querySelector('form');
 const apiKey = '48cc82e8f8c02a45326449bdff50a9b3';
-let coord = [];
+let coord;
+let units;
 
 async function geoApi(city){
     const geoURL = await `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
     const geoData = await fetch(geoURL);
     const geoResult = await geoData.json();
     console.log(geoResult);
+
+    if(geoResult.length == 0){
+        p.textContent = "Please enter a valid city";
+        return;
+    }
+
     //add logic here to be able to select which of the same name cities they want
     if (geoResult.length > 1){
         console.log("more than one");
     }
     //
+
     geoResult.forEach(att => {
         var lat = att.lat;
         var lon = att.lon;
@@ -25,13 +33,19 @@ async function geoApi(city){
 
 async function weather(){
     console.log(coord);
-    const apiURL = await `https://api.openweathermap.org/data/2.5/weather?lat=${coord[0]}&lon=${coord[1]}&appid=${apiKey}&units=imperial`;
+    const apiURL = await `https://api.openweathermap.org/data/2.5/weather?lat=${coord[0]}&lon=${coord[1]}&appid=${apiKey}&units=${units}`;
+    console.log(units);
     const data = await fetch(apiURL);
     const result = await data.json();
     console.log(result);
 
     try{
         p.textContent = result.main.temp;
+        if (units == 'metric'){
+            p.textContent = `${result.main.temp}°C`;
+        } else{
+            p.textContent = `${result.main.temp}°F`;
+        }
     }
 
     catch(err){
@@ -39,12 +53,20 @@ async function weather(){
     }
 
     finally{
-        console.log('finally block');
+        units = undefined;
     }
 }
 
 form.addEventListener(("submit"), (e) => {
     e.preventDefault();
     coord = [];
+    if(document.getElementById('metric').checked){
+        units = 'metric';
+    } else if(document.getElementById('imperial').checked){
+        units = 'imperial';
+    } else{
+        p.textContent = "Please select a unit";
+        return;
+    }
     geoApi(cityInput.value);
 })
